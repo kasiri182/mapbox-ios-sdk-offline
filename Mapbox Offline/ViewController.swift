c710970707720d4e902e166e6f428935325a3744
Justin R. Miller <incanus@codesorcery.net>
2015-04-12 22:08:34 -0700

    default to white view

--- Mapbox Offline/ViewController.swift
+++ Mapbox Offline/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController,
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.view.backgroundColor = UIColor.whiteColor()
+
         self.title = baseTitle
 
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Trash,
